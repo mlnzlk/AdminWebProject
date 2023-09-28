8d81a04e34864359c506f46a9083d084a11cd075
Minji-Kwon <93921790+mlnzlk@users.noreply.github.com>
2023-09-28 20:58:11 +0900

    addmenu페이지 스타일/로직 파일 분리

--- src/pages/AddMenu/AddMenuStyles.js
+++ src/pages/AddMenu/AddMenuStyles.js
@@ -13,47 +13,76 @@ export const StyledInput = styled.input`
   padding-left :29px; 
   border: 1px solid #CCCCCC;
   border-radius: 16px;
-  
+  margin-left: 24px;
+
 &::placeholder { /* Chrome, Firefox, Opera, Safari */
     color: gray;
  }
 `;
 
 export const Select = styled.select`
-margin-left:8px;padding-left:5px;display:flex;color:black;justify-content:center;align-items:center;font-size:
-20px;background-color:white;border-radius:16px;width:
-197px;height:
-62px;padding-left :15px;border:
-1.0 solid #CCCCCC;`;
+margin-left:8px;
+padding-left:5px;
+display:flex;
+color:black;
+justify-content:center;
+align-items:center;
+font-size:20px;
+background-color:white;
+border-radius:16px;
+width:197px;
+height:62px;
+padding-left :15px;
+border:1.0px solid #CCCCCC;`
+
+;
 
 export const Container2 = styled.div`
-display:flex;justify-content:center;align-items:center;width:
-247;height:
-62;border-radius:
-16;margin-left :10;border :
-1.0 solid #CCCCCC;;`;
+display:flex;
+justify-content:center;
+align-items:center;
+width:247px;
+height:62px;
+border-radius:16px;
+margin-left :10px;
+border :1.0px solid #CCCCCC;
+`;
 
 export const Button1 = styled.button`
-font-size :
-20;display:flex ;justify-content:center ;align-items :center;width :
-123.5;height :
-62;border:none;background-color:${props => props.clicked ? '#F0F2FB' : '#FFFFFF'} ;
-border-radius :
-16 ;transition:.5s;;`;
+font-size :20px;
+display:flex ;
+justify-content:center 
+;align-items :center;
+width :123.5px;
+height :62px;
+border:none;
+background-color:${props => props.clicked ? '#F0F2FB' : '#FFFFFF'} ;
+border-radius :16px ;
+transition:.5s;
+`;
 
 export const Button2 = styled.button`
-font-size :
-20;display:flex ;justify-content:center ;align-items :center;width :
-123.5;height :
-62;border:none;background-color:${props => props.clicked ? '#FDE9E5' : '#FFFFFF'} ;
-border-radius :
-16 ;transition:.5s;;`;
+font-size :20px;
+display:flex ;
+justify-content:center ;
+align-items :center;
+width :123.5px;
+height :62px;
+border:none;
+background-color:${props => props.clicked ? '#FDE9E5' : '#FFFFFF'} ;
+border-radius :16px ;
+transition:.5s;
+`;
 
 export const Button3 = styled.button`
-width :238 ;
-height :62 ;
+width :238px ;
+height :62px ;
 background:#D9D9D9 ;
-border-radius :16 ; border:none ;
-transition:.5s;margin-left10;padding-
-left24;text-align:left;font-size
-20 ;;`;
+border-radius :16px ; 
+border:none ;
+transition:.5s;
+margin-left : 10px;
+padding-left: 24px;
+text-align:left;
+font-size: 20px ;
+`;
