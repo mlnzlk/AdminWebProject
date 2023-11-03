3483b6da313a4bde2a9cb253119488393a38f422
Minji-Kwon <93921790+mlnzlk@users.noreply.github.com>
2023-11-03 18:04:57 +0900

    재료삭제시 서버에 전송

--- src/pages/EditMenu/EditMenu.js
+++ src/pages/EditMenu/EditMenu.js
@@ -22,203 +22,218 @@ import axios from 'axios';
 
 export default function EditMenu() {
   const [socket, setSocket] = useState(null); // 상태변수로 socket 외부에서도 관리
-
-
+  
   const [menuName, setMenuName] = useState('');
   const { productId } = useParams();
   const [menuData, setMenuData] = useState(null);
   const [categoryId, setCategoryId] = useState('1'); // 기본값을 'category1'로 설정
   const [imageList, setImageList] = useState([]);
-
-  const [updatedMenuData, setUpdatedMenuData] = useState(null); // 업데이트된 메뉴데이터 전역변수
-
-    const navigate = useNavigate();
     
-    const [isButton1Clicked, setButton1Clicked] = useState(false);
-    const [isButton2Clicked, setButton2Clicked] = useState(false);
-
-    const [ingredientModalOpen, setIngredientModalOpen] = useState(false);
-    
-    // 각 판매가 버튼에 대해 별도의 모달 상태값 생성
-    const [numberWonModalOpen1, setNumberWonModalOpen1] = useState(false);
-    const [numberWonModalOpen2, setNumberWonModalOpen2] = useState(false);
-    const [numberWonModalOpen3, setNumberWonModalOpen3] = useState(false);
-    const [numberWonModalOpen4, setNumberWonModalOpen4] = useState(false);
-
-
-    // 각 가격에 대한 상태값을 생성
-    const [price1, setPrice1] = useState("");
-    const [price2, setPrice2] = useState("");
-    const [price3, setPrice3] = useState("");
-    const [price4, setPrice4] = useState("");
-    
-    // 재료추가 모달창 상태값 생성
-    const [ModalCancelRegisterMenuOpen, setModalCancelRegisterMenu] = useState(false);
-
-    // 메뉴삭제 모달창 상태값 생성 -> 이 코드 메뉴수정.js에 옮기기
-    const [ModalDeleteMenuOpen, setDeleteMenu] = useState(false);
-    const handleButton1Click = () => {
-      if (!isButton1Clicked) {
-        setButton1Clicked(true);
-        setButton2Clicked(false);
-      }
-    };
+  const navigate = useNavigate();
   
-    const handleButton2Click = () => {
-      if (!isButton2Clicked) {
-        setButton1Clicked(false);
-        setButton2Clicked(true);
-      }
-    };
-
-
-    const url = menuData && menuData.url;
-    const fileName = url ? url.substring(url.lastIndexOf("/") + 1) : "";  
-    const [selectedFile, setSelectedFile] = useState(null);
-
-    const handleFileInputChange = (event) => {
-      const selectedFile = event.target.files[0];  // 파일 선택 후 selectedFile 상태를 업데이트하고, 라벨의 내용을 파일 이름으로 변경하도록 조건부 렌더링을 사용
-      if (selectedFile) {        
-        console.log("선택한 파일:", selectedFile);
-        setSelectedFile(selectedFile); // 선택한 파일을 selectedFile 상태 변수에 저장
-        setImageList(prevImageList => [...prevImageList, selectedFile]); // 파일 선택 시 selectedFile 상태 업데이트
-      }
-    };
-
-    useEffect(() => {
-
-      
-      if (productId) {
-        axios.get(`http://robros-alb-590302301.ap-northeast-2.elb.amazonaws.com/api/v1/recipes/${productId}`)
-          .then((response) => {
-            console.log(response.data); // 받아온 데이터 확인용 로그
-            
-            setMenuData(response.data);  // setMenuData로 데이터 설정
-            setMenuName(response.data.name); // name 값을 menuName 상태 변수에 설정 -> <Input1> 컴포넌트가 렌더링될 때 초기값으로 해당 이름이 표시
-          
-            // categoryId 값을 설정
-          if (response.data.categoryId === 1) {
-            setCategoryId('1');
-          } else if (response.data.categoryId === 2) {
-            setCategoryId('2');
-          } else if (response.data.categoryId === 3) {
-            setCategoryId('3');
-          } else if (response.data.categoryId === 4) {
-            setCategoryId('4');
-          }
-
-                  // 첫 번째 레시피의 크기를 확인하고 초기 버튼 상태를 설정
-                  if (response.data.recipes && response.data.recipes.length > 0) {
-                    // Set prices based on recipe data
-                    if (response.data.recipes[0].size === '8') {
-                      setButton1Clicked(true);
-                      setButton2Clicked(false);
-                      setPrice1(response.data.recipes[0].price.toString());
-                      setPrice2(response.data.recipes[1].price.toString());
-                      setPrice3(response.data.recipes[2].price.toString());
-
-                    } else if (response.data.recipes[0].size === '4') {
-                      setButton1Clicked(false);
-                      setButton2Clicked(true);
-                      setPrice1(response.data.recipes[0].price.toString());
-                      setPrice2(response.data.recipes[1].price.toString());
-                      setPrice3(response.data.recipes[2].price.toString());
-
-                    }
-                  }
-                })
-                
-                .catch((error) => console.error(`Error!!!: ${error}`));
-            }
-
-            
-
-            const socketInstance = new WebSocket('ws://192.168.0.19:12345');
-
-            // 연결이 열릴 때 실행될 이벤트 리스너
-            socketInstance.onopen = function (event) {
-              console.log('WebSocket 연결 성공');
-                setSocket(socketInstance); // socket 상태 업데이트
-        
-            };
-        
-            // 메시지 수신 이벤트 리스너
-            socketInstance.onmessage = function (event) {
-              console.log(`Received message from server : ${event.data}`);
-            };
-        
-            return () => {
-              if (socketInstance) {
-                  socketInstance.close();
-              }
-              
-              setSocket(null); // 컴포넌트 unmount 시에는 소켓 인스턴스 제거
-          };
-
-
-          }, [productId]);
-
-
-          // 저장하기 버튼 클릭시 이미지 정송, 수정된 json 파일 전송
-          const handleSave = async () => {
-            const formData = new FormData(); 
-          
-            imageList.forEach(image => {
-              formData.append('img', image);
-            });
-            
-          
-            formData.append(
-              'request',
-              // JSON.stringify({updatedMenuData}),
-              new Blob([JSON.stringify(menuData)], {type: 'application/json'})
-            );
-          
-            try {
-              await axios.patch(
-                'http://robros-alb-590302301.ap-northeast-2.elb.amazonaws.com/api/v1/recipe', 
-                formData, 
-                {
-                headers: {'Content-Type': 'multipart/form-data'}
-                })
-              .then(response => {
-                console.log(response.data);
-
-                navigate('/menumanagement');
-              })
-              .catch(error => { 
-                console.error(`Error updating data: ${error}`);
-              });
-            } catch (error) {
-              console.error(error);
-            }
-            console.log("Updated menu data:", menuData);
-            console.log("Updated img file:", imageList);
-          };
-          
-          
+  const [isButton1Clicked, setButton1Clicked] = useState(false);
+  const [isButton2Clicked, setButton2Clicked] = useState(false);
+  
+  const [ingredientModalOpen, setIngredientModalOpen] = useState(false);
+  
+  // 각 판매가 버튼에 대해 별도의 모달 상태값 생성
+  const [numberWonModalOpen1, setNumberWonModalOpen1] = useState(false);
+  const [numberWonModalOpen2, setNumberWonModalOpen2] = useState(false);
+  const [numberWonModalOpen3, setNumberWonModalOpen3] = useState(false);
+  const [numberWonModalOpen4, setNumberWonModalOpen4] = useState(false);
+  
+  
+  // 각 가격에 대한 상태값을 생성
+  const [price1, setPrice1] = useState("");
+  const [price2, setPrice2] = useState("");
+  const [price3, setPrice3] = useState("");
+  const [price4, setPrice4] = useState("");
+  
+  // 재료추가 모달창 상태값 생성
+  const [ModalCancelRegisterMenuOpen, setModalCancelRegisterMenu] = useState(false);
+  
+  // 메뉴삭제 모달창 상태값 생성 -> 이 코드 메뉴수정.js에 옮기기
+  const [ModalDeleteMenuOpen, setDeleteMenu] = useState(false);
+  const handleButton1Click = () => {
+    if (!isButton1Clicked) {
+      setButton1Clicked(true);
+      setButton2Clicked(false);
+    }
+  };
+  
+  const handleButton2Click = () => {
+    if (!isButton2Clicked) {
+      setButton1Clicked(false);
+      setButton2Clicked(true);
+    }
+  };
+  
+  
+  const url = menuData && menuData.url;
+  const fileName = url ? url.substring(url.lastIndexOf("/") + 1) : "";  
+  const [selectedFile, setSelectedFile] = useState(null);
+  
+  const handleFileInputChange = (event) => {
+    const selectedFile = event.target.files[0];  // 파일 선택 후 selectedFile 상태를 업데이트하고, 라벨의 내용을 파일 이름으로 변경하도록 조건부 렌더링을 사용
+    if (selectedFile) {        
+      console.log("선택한 파일:", selectedFile);
+      setSelectedFile(selectedFile); // 선택한 파일을 selectedFile 상태 변수에 저장
+      setImageList(prevImageList => [...prevImageList, selectedFile]); // 파일 선택 시 selectedFile 상태 업데이트
+    }
+  };
+  
+  useEffect(() => {
+  
     
-          const handleConfirm = (selectedData) => {
-            console.log("Selected data from modal:", selectedData);
+    if (productId) {
+      axios.get(`http://robros-alb-590302301.ap-northeast-2.elb.amazonaws.com/api/v1/recipes/${productId}`)
+        .then((response) => {
+          console.log(response.data); // 받아온 데이터 확인용 로그
           
-            const updatedMenuData = {
-              ...menuData,
-              recipes: menuData.recipes.map((recipe) => {
-                const updatedIngredients = recipe.ingredient
-                  ? [...recipe.ingredient, ...selectedData.map((data, index) => ({ ...data, seq: index + recipe.ingredient.length }))]
-                  : [...selectedData];
-                return {
-                  ...recipe,
-                  ingredient: updatedIngredients
-                };
+          setMenuData(response.data);  // setMenuData로 데이터 설정
+          setMenuName(response.data.name); // name 값을 menuName 상태 변수에 설정 -> <Input1> 컴포넌트가 렌더링될 때 초기값으로 해당 이름이 표시
+        
+          // categoryId 값을 설정
+        if (response.data.categoryId === 1) {
+          setCategoryId('1');
+        } else if (response.data.categoryId === 2) {
+          setCategoryId('2');
+        } else if (response.data.categoryId === 3) {
+          setCategoryId('3');
+        } else if (response.data.categoryId === 4) {
+          setCategoryId('4');
+        }
+  
+                // 첫 번째 레시피의 크기를 확인하고 초기 버튼 상태를 설정
+                if (response.data.recipes && response.data.recipes.length > 0) {
+                  // Set prices based on recipe data
+                  if (response.data.recipes[0].size === '8') {
+                    setButton1Clicked(true);
+                    setButton2Clicked(false);
+                    setPrice1(response.data.recipes[0].price.toString());
+                    setPrice2(response.data.recipes[1].price.toString());
+                    setPrice3(response.data.recipes[2].price.toString());
+  
+                  } else if (response.data.recipes[0].size === '4') {
+                    setButton1Clicked(false);
+                    setButton2Clicked(true);
+                    setPrice1(response.data.recipes[0].price.toString());
+                    setPrice2(response.data.recipes[1].price.toString());
+                    setPrice3(response.data.recipes[2].price.toString());
+  
+                  }
+                }
               })
-            };
+              
+              .catch((error) => console.error(`Error!!!: ${error}`));
+          }
+  
           
-            setMenuData(updatedMenuData);
-            console.log("Updated menu data:", updatedMenuData);
+  
+          const socketInstance = new WebSocket('ws://192.168.0.19:12345');
+  
+          // 연결이 열릴 때 실행될 이벤트 리스너
+          socketInstance.onopen = function (event) {
+            console.log('WebSocket 연결 성공');
+              setSocket(socketInstance); // socket 상태 업데이트
+      
           };
+      
+          // 메시지 수신 이벤트 리스너
+          socketInstance.onmessage = function (event) {
+            console.log(`Received message from server : ${event.data}`);
+          };
+      
+          return () => {
+            if (socketInstance) {
+                socketInstance.close();
+            }
+            
+            setSocket(null); // 컴포넌트 unmount 시에는 소켓 인스턴스 제거
+        };
+  
+  
+        }, [productId]);
+  
+  
+        // 저장하기 버튼 클릭시 이미지 정송, 수정된 json 파일 전송
+        const handleSave = async () => {
+          const formData = new FormData();
+        
+          imageList.forEach(image => {
+            formData.append('img', image);
+          });
+        
+          formData.append(
+            'request',
+            new Blob([JSON.stringify(menuData)], { type: 'application/json' })
+          );
+        
+          try {
+            await axios.patch(
+              'http://robros-alb-590302301.ap-northeast-2.elb.amazonaws.com/api/v1/recipe',
+              formData,
+              {
+                headers: { 'Content-Type': 'multipart/form-data' }
+              }
+            );
+            navigate('/menumanagement');
+          } catch (error) {
+            console.error(`Error updating data: ${error}`);
+          }
+        };
+        
+        
+  
+        const handleConfirm = (selectedData) => {
+          console.log("Selected data from modal:", selectedData);
+        
+          const updatedMenuData = {
+            ...menuData,
+            recipes: menuData.recipes.map((recipe) => {
+              const updatedIngredients = recipe.ingredient
+                ? [...recipe.ingredient, ...selectedData.map((data, index) => ({ ...data, seq: index + recipe.ingredient.length }))]
+                : [...selectedData];
+              return {
+                ...recipe,
+                ingredient: updatedIngredients
+              };
+            })
+          };
+        
+          setMenuData(updatedMenuData);
+          console.log("Updated menu data:", updatedMenuData);
+        };
+        
+        const handleRemoveIngredient = (index) => {
+          setMenuData(prevData => {
+            const updatedRecipes = prevData.recipes.map(recipe => {
+              const updatedIngredient = recipe.ingredient.map(ing => {
+                if (ing.seq === prevData.recipes[0].ingredient[index].seq) {
+                  return {
+                    ...ing,
+                    isDeleted: true
+                  };
+                }
+                return ing;
+              });
+              return {
+                ...recipe,
+                ingredient: updatedIngredient
+              };
+            });
+        
+            const updatedMenuData = {
+              ...prevData,
+              recipes: updatedRecipes
+            };
+            setMenuData(updatedMenuData); // 클라이언트 측에서 menuData 업데이트
+            return updatedMenuData;
+          });
+        };
+        
           
-
+          
     return (
       
       <div> 
@@ -338,45 +353,51 @@ export default function EditMenu() {
         }}
 >           
 {menuData &&
-  menuData.recipes &&  
-  menuData.recipes[0].ingredient.map((ing, index) => (
-    <ListContent key={index}>
-      <img style={{ margin: "0 0 0 15px" }} src={Drag} />
-      <label style={{ marginLeft: "14px", fontFamily: "Pretendard", fontSize: '20px', width: "418px", height: "40px", background: "#FFFFFF", borderRadius: "8px", paddingLeft:'24px', paddingTop:'16px'}}>
-        {ing.ingredientName}
-      </label>
-      {menuData.recipes.map(recipe => {
-        const ingredient = recipe.ingredient.find(i => i.seq === ing.seq);
-        return (
-          <>
-<Button11
-  key={recipe.cupId}
-  zeroQuantity={ingredient && ingredient.quantity === 0}
-  onClick={() => setNumberWonModalOpen4(true)}
->
-  {ing.ingredientName === '샷' ? (numberWonModalOpen4 ? price4 + '샷' : ingredient.quantity + '샷') : (numberWonModalOpen4 ? price4 + 'ml' : (ingredient ? ingredient.quantity + 'ml' : '0'))}
-</Button11>
+  menuData.recipes &&
+  menuData.recipes[0].ingredient.map((ing, index) => {
+    // isDeleted 속성이 true인 경우 건너뛰기
+    if (ing.isDeleted) {
+      return null;
+    }
 
-{numberWonModalOpen4 && (
-  <ModalNumber_Won_edit
-    closeModal={setNumberWonModalOpen4}
-    setValue={value => {
-      setPrice4(value.toString()); // Update the price4 with the new value entered in the modal
-      setNumberWonModalOpen4(false); // Close the modal
-    }}
-  />
-)}
-          </>
-        );
-      })}
-      <ButtonX >
-        <img style={{ margin:"10" }} src={Del} /> 
-      </ButtonX> 
-    </ListContent>
-))}
+    return (
+      <ListContent key={index}>
+        <img style={{ margin: "0 0 0 15px" }} src={Drag} />
+        <label style={{ marginLeft: "14px", fontFamily: "Pretendard", fontSize: '20px', width: "418px", height: "40px", background: "#FFFFFF", borderRadius: "8px", paddingLeft:'24px', paddingTop:'16px'}}>
+          {ing.ingredientName}
+        </label>
+        {menuData.recipes.map(recipe => {
+          const ingredient = recipe.ingredient.find(i => i.seq === ing.seq);
+          return (
+            <>
+              <Button11
+                key={recipe.cupId}
+                zeroQuantity={ingredient && ingredient.quantity === 0}
+                onClick={() => setNumberWonModalOpen4(true)}
+              >
+                {ing.ingredientName === '샷' ? (numberWonModalOpen4 ? price4 + '샷' : ingredient.quantity + '샷') : (numberWonModalOpen4 ? price4 + 'ml' : (ingredient ? ingredient.quantity + 'ml' : '0'))}
+              </Button11>
 
-
+              {numberWonModalOpen4 && (
+                <ModalNumber_Won_edit
+                  closeModal={setNumberWonModalOpen4}
+                  setValue={value => {
+                    setPrice4(value.toString());
+                    setNumberWonModalOpen4(false);
+                  }}
+                />
+              )}
+            </>
+          );
+        })}
+        <ButtonX onClick={() => handleRemoveIngredient(index)}>
+          <img style={{ margin:"10" }} src={Del} /> 
+        </ButtonX> 
+      </ListContent>
+    );
+  })}
 </ContainerList>
+
 
 
       <div style={{ display: 'flex', alignItems: 'center' }}>
